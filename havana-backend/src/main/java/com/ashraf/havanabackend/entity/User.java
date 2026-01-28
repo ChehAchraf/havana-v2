174451b76992ac714259689d24b8da932ef89583
chehachraf <achrafchehboun@outlook.com>
2026-01-28 14:07:44 +0100

    complete the user entity with role enum

--- havana-backend/src/main/java/com/ashraf/havanabackend/entity/User.java
+++ havana-backend/src/main/java/com/ashraf/havanabackend/entity/User.java
@@ -1,4 +1,31 @@
 package com.ashraf.havanabackend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true)
+    private String username;
+    
+    @Column(unique = true)
+    private String email;
+    
+    
+    private String password;
+    
+    private String role;
+
 }
---
+++ havana-backend/src/main/java/com/ashraf/havanabackend/enums/Role.java
@@ -0,0 +1,6 @@
+package com.ashraf.havanabackend.enums;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER
+}
